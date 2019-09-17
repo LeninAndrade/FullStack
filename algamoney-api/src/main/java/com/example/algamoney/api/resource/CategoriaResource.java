bc7d52d3db1655670c1182c45768b9af96208fad
Lenin Andrade <lenin.88@hotmail.com>
2019-09-17 10:43:02 -0300

    Retornar 404 caso não exista a categoria

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -6,21 +6,26 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
 
+
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
+	
+//	private static final Object ResponseStatus = null;
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -40,9 +45,23 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
 	
+//	@GetMapping("/{codigo}")
+//	@ResponseStatus(HttpStatus.NOT_FOUND)
+//	public Object buscarPeloCodigo(@PathVariable Long codigo) {
+//		Categoria buscarCategoria = categoriaRepository.findById(codigo).orElse(null);
+//		if (buscarCategoria == null) {
+//			return ResponseStatus;
+//		}
+//		else
+//			return buscarCategoria;
+//	}
+	
+	@SuppressWarnings("rawtypes")
 	@GetMapping("/{codigo}")
-	public Object buscarPeloCodigo(@PathVariable Long codigo) {
-		return categoriaRepository.findById(codigo);
+	public ResponseEntity buscarPeloCodigo(@PathVariable Long codigo) {
+		return this.categoriaRepository.findById(codigo)
+				.map(categoria -> ResponseEntity.ok(categoria))
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 }
