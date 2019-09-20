907c49b5f613597c3a601fcc0f3794f17eaf3f07
Lenin Andrade <lenin.88@hotmail.com>
2019-09-20 12:37:17 -0300

    Implementação do método para cadastro de novo lançamento

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -3,15 +3,23 @@ package com.example.algamoney.api.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.LancamentoRepository;
 
 @RestController
@@ -20,6 +28,9 @@ public class LancamentoResource {
 
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
 	public List<Lancamento> listar() {
@@ -33,6 +44,15 @@ public class LancamentoResource {
 		return lancamento.isPresent() ? ResponseEntity.ok(lancamento.get()) :  ResponseEntity
 																					.status(HttpStatus.NOT_FOUND)
 																					.build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> cadastrarLancamento(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento lancamentoCadastrado = lancamentoRepository.save(lancamento);
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoCadastrado.getCodigo()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoCadastrado);
 	}
 }
 
