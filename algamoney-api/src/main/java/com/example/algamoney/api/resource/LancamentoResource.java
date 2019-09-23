12cd8c7716238f1dc3171cde2f40a58545d8280c
Lenin Andrade <lenin.88@hotmail.com>
2019-09-23 15:14:26 -0300

    Implementando pesquisa de lançamento com metamodel

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -25,6 +25,7 @@ import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.exceptionhandler.AlgamoneyExceptionHandler.Erro;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.filter.LancamentoFilter;
 import com.example.algamoney.api.service.LancamentoService;
 import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 
@@ -45,8 +46,8 @@ public class LancamentoResource {
 	private MessageSource messageSource;
 	
 	@GetMapping
-	public List<Lancamento> listar() {
-		return lancamentoRepository.findAll();
+	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
+		return lancamentoRepository.filtrar(lancamentoFilter);
 	}
 	
 	@GetMapping("/{codigo}")
