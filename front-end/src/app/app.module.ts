5dc6f944879e506ccfd5c7b5fd9bfd6a53775804
Lenin Andrade <lenin.88@hotmail.com>
2019-10-03 16:56:54 -0300

    Instalando a biblioteca CSS do bootstrap

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    BemVindoComponent
   ],
   imports: [
     BrowserModule
